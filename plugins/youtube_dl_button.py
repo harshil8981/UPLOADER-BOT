3d44ac236356d1bd1cdb9c2c69aeed65f55905d4
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2022-05-27 19:50:58 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modified By > @DC4_WARRIOR
+# (c) Harshil H P | Modified By > @harshil8981
 
 # the logging things
 import logging
