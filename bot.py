c67f04f467f95fdd71e9be58eab9c445acd4d9b7
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2022-05-27 19:57:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
+# (c) Harshil H P | Modifieded By : @Harshil8981
 
 import os
 from config import Config
-from pyrogram import Client as Clinton
+from pyrogram import Client as Harshil
 
 if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = Clinton("@UPLOADER_X_BOT",
+    Warrior = Harshil("@UPLOADER_HP_BOT",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
